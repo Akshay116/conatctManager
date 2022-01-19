622242121e804761ffe52a287c863c9de8e69992
akshay padmane <akshaypadmane@akshays-MacBook-Air.local>
2022-01-19 09:24:21 +0530

    Aded symantic ui and form

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
+import React from "react";
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+import '../App.css';
 
 
 function App() {
   return (
-    <div className="App">
-     CONTCT
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList />
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contactlist = () => {
+    return (
+        <div className='ui celled list'>
+            Contactlist
+            
+        </div>
+    );
+}
+
+export default Contactlist;
